72a7c5e134af926935c7f4d27d96d31123f89787
Ben Martin <N/A>
2016-02-27 10:18:01 -0600

    added reminder class (includes parser and serializer), added warn time to alarm builder

--- AlarmBuilder.py
+++ AlarmBuilder.py
@@ -5,7 +5,9 @@ from os import environ
 from os.path import join
 from Tix import *
 from tkFileDialog import *
-import tkFont
+import Reminder
+from Reminder import FONTS
+import tkMessageBox
 
 CONFIG_DIR = join(environ['HOME'], '.super_reminder/')
 DAYS = 'Monday Tuesday Wednesday Thursday Friday Saturday Sunday'.split()
@@ -13,14 +15,9 @@ SIZES = [6, 7, 8, 9, 10, 11, 12, 14, 16, 18, 21, 24, 36, 48, 60, 72, 80, 88, 96]
 
 """
 TODO:
-    Figure out style strings for alarms
-        should they always be line 2?
-        should they be optionally tagged onto the time line?
-    opening AlarmBuilder creates the hidden directory
-    opening a file in Alarm builder imports ALL of its settings
     make about page
-    save as and saves actually save ALL values to the file in the correct format
-        be sure to use everyday if all days are selected
+    warn if quiting and unsaved
+    show if edited but unsaved
         
 OPTIONAL:
     maybe allow am and pm format?
@@ -55,7 +52,8 @@ class Builder(Tk):
         Tk.__init__(self, *args, **kwargs)
         
         self.wm_title("Alarm Builder")
-                    
+        self._current_file = None
+        
         ############
         # Menu Bar #
         ############
@@ -63,24 +61,43 @@ class Builder(Tk):
         self.menu_bar = Menu(self)
         
         file_menu = Menu(self.menu_bar, tearoff=0)
-        file_menu.add_command(label="New", command=self.new)
+        file_menu.add_command(label="New", command=self.new, accelerator="Ctrl+N")
         file_menu.add_separator()
-        file_menu.add_command(label="Open", command=self.open)
+        file_menu.add_command(label="Open", command=self.open, accelerator="Ctrl+O")
         file_menu.add_separator()
-        file_menu.add_command(label="Save", command=self.save)
+        file_menu.add_command(label="Save", command=self.save, accelerator="Ctrl+S")
         file_menu.add_command(label="Save As", comman=self.save_as)
         file_menu.add_separator()
-        file_menu.add_command(label="Quit", command=self.quit)
+        file_menu.add_command(label="Quit", command=self.quit, accelerator="Ctrl+Q")
         self.file_menu = file_menu
+        
+        edit_menu = Menu(self.menu_bar, tearoff=0)
+        edit_menu.add_command(label="Undo", command=lambda: self.focus_get().event_generate('<<Undo>>'), accelerator="Ctrl+Z")
+        edit_menu.add_command(label="Redo", command=lambda: self.focus_get().event_generate('<<Redo>>'), accelerator="Ctrl+Y")
+        edit_menu.add_separator()
+        edit_menu.add_command(label="Cut", command=lambda: self.focus_get().event_generate('<<Cut>>'), accelerator="Ctrl+X")
+        edit_menu.add_command(label="Copy", comman=lambda: self.focus_get().event_generate('<<Copy>>'), accelerator="Ctrl+C")
+        edit_menu.add_command(label="Paste", comman=lambda: self.focus_get().event_generate('<<Paste>>'), accelerator="Ctrl+V")
+        self.edit_menu = file_menu
         
         help_menu = Menu(self.menu_bar, tearoff=0)
         help_menu.add_command(label="About", command=self.about)
         self.help_menu = help_menu
         
         self.menu_bar.add_cascade(label="File", menu=file_menu)
+        self.menu_bar.add_cascade(label="Edit", menu=edit_menu)
         self.menu_bar.add_cascade(label="Help", menu=help_menu)
         
         self.config(menu=self.menu_bar)
+        
+        #################
+        # Menu Keybinds #
+        #################
+        
+        self.bind_all('<Control-n>', self.new)
+        self.bind_all('<Control-o>', self.open)
+        self.bind_all('<Control-s>', self.save)
+        self.bind_all('<Control-q>', lambda *_: self.quit())
         
         ###########
         # Widgets #
@@ -94,13 +111,15 @@ class Builder(Tk):
         self.hour_entry = ComboBox(self.entry_centered_frame, label="Hour: ", variable=self.hour, dropdown=1, editable=1)
         self.minute_entry = ComboBox(self.entry_centered_frame, label="Minute: ", variable=self.minute, dropdown=1, editable=1)
         
-        self.day_chooser = Listbox(self.entry_centered_frame, selectmode=EXTENDED, height=7, width=(len(max(DAYS, key=len)) + 1))
+        self.day_chooser = Listbox(self.entry_centered_frame, selectmode=EXTENDED, exportselection=0, height=7, width=(len(max(DAYS, key=len)) + 1))
         
-        fonts = sorted([x for x in tkFont.families() if not x.startswith('@')])
         self.font_name = StringVar()
         self.font_size = StringVar()
         self.font_name_chooser = ComboBox(self.entry_centered_frame, label="Font: ", dropdown=1, editable=0, variable=self.font_name)
         self.font_size_chooser = ComboBox(self.entry_centered_frame, label="Size:  ", dropdown=1, editable=1, variable=self.font_size)
+        
+        self.warning_time = StringVar()
+        self.warning_time_chooser = ComboBox(self.entry_centered_frame, label="Warning Minutes", dropdown=1, editable=1, variable=self.warning_time)
         
         ############
         # Defaults #
@@ -108,18 +127,21 @@ class Builder(Tk):
 
         self.hour.set('12')
         self.minute.set('00')
-        self.font_name.set(fonts[0])
+        self.font_name.set(FONTS[0])
         self.font_size.set('12')
+        self.warning_time.set('5')
         for day in DAYS:
             self.day_chooser.insert(END, day)
         for hour in range(0,24):
             self.hour_entry.insert(END, str(hour).zfill(2))
         for minute in range(0, 60):
             self.minute_entry.insert(END, str(minute).zfill(2))
-        for font in fonts:
+        for font in FONTS:
             self.font_name_chooser.insert(END, font)
         for size in SIZES:
             self.font_size_chooser.insert(END, size)
+        for minute in range(0, 121):
+            self.warning_time_chooser.insert(END, str(minute))
         
         ###########################
         # Text Entry Box Specific #
@@ -130,7 +152,7 @@ class Builder(Tk):
         self.message_entry_frame.grid_rowconfigure(0, weight=1)
         self.message_entry_frame.grid_columnconfigure(0, weight=1)
         
-        self.message_entry = CenteredText(self.message_entry_frame, callback=self._recenter)
+        self.message_entry = CenteredText(self.message_entry_frame, callback=self._recenter, wrap=WORD, undo=True)
         self.message_entry.tag_configure("center-tag", justify='center')
         self.message_entry.tag_add("center-tag", 1.0, END)
         
@@ -143,11 +165,12 @@ class Builder(Tk):
 
         self.entry_centered_frame.pack()
         
-        self.day_chooser.grid(      row=0, column=0, sticky="nsew", rowspan=2)
-        self.hour_entry.grid(       row=0, column=1, sticky="nsew", padx=10)
-        self.minute_entry.grid(     row=0, column=2, sticky="nsew", padx=10)
-        self.font_name_chooser.grid(row=1, column=1, sticky="nsew", padx=10)
-        self.font_size_chooser.grid(row=1, column=2, sticky="nsew", padx=10)
+        self.day_chooser.grid(         row=0, column=0, sticky="nsew", rowspan=3)
+        self.hour_entry.grid(          row=0, column=1, sticky="nsew", padx=10)
+        self.minute_entry.grid(        row=0, column=2, sticky="nsew", padx=10)
+        self.font_name_chooser.grid(   row=1, column=1, sticky="nsew", padx=10)
+        self.font_size_chooser.grid(   row=1, column=2, sticky="nsew", padx=10)
+        self.warning_time_chooser.grid(row=2, column=1, sticky="nsew", padx=10)
         
         self.message_entry.grid(row=0, column=0, sticky="nsew")
         
@@ -159,6 +182,7 @@ class Builder(Tk):
         self.font_size.trace("w", self._font_change)
         self.hour.trace("w", self._time_change)
         self.minute.trace("w", self._time_change)
+        self.warning_time.trace("w", self._warning_time_change)
         
     def new(self, *args, **kwargs):
         self.message_entry.delete(1.0, END)
@@ -167,28 +191,62 @@ class Builder(Tk):
         
     def open(self, *args, **kwargs):
         file_path = askopenfilename(initialdir=CONFIG_DIR)
-        if file_path:
-            self.message_entry.delete(1.0, END)
-            with open(file_path) as f:
-                contents = f.readlines()
-            message = ''.join(contents[1:])
-            self.message_entry.insert(1.0, message, 'center-tag')
-            self._current_file = file_path
-            self.wm_title("Alarm Builder: %s" % self._current_file)
+        if not file_path:
+            return
         
+        self.message_entry.delete(1.0, END)
+        reminder = Reminder.parse_file(file_path)
+        
+        self.message_entry.insert(1.0, reminder.text, 'center-tag')
+        
+        self.hour.set(str(reminder.time.hour).zfill(2))
+        self.minute.set(str(reminder.time.minute).zfill(2))
+        
+        self.day_chooser.selection_clear(0, END)
+        for day in reminder.days:
+            self.day_chooser.selection_set(DAYS.index(day.capitalize()))
+        
+        self.font_name.set(reminder.font[0])
+        self.font_size.set(reminder.font[1])
+        
+        self.warning_time.set(str(reminder.warn))
+        
+        self._current_file = file_path
+        self.wm_title("Alarm Builder: %s" % self._current_file)
+    
     def save(self, *args, **kwargs):
-        if self._current_file:
-            pass
-        else:
-            self.save_as(*args, **kwargs)
+        if not self._current_file:
+            self.save_as()
+            return
+
+        hours = int(self.hour.get())
+        minutes = int(self.minute.get())
+        time = Reminder.make_time(hours, minutes)
+        
+        selection = map(int, self.day_chooser.curselection())
+        days = [self.day_chooser.get(day) for day in selection]
+        
+        text = self.message_entry.get(1.0, END)
+        warn = int(self.warning_time.get())
+        font = (self.font_name.get(), int(self.font_size.get()))
+        
+        if not days:
+            tkMessageBox.showerror("Save file", "Cannt save without at least 1 day selected")
+            return
+        
+        reminder = Reminder.Reminder(time, days, text, warn, font)
+        Reminder.serialize_file(reminder, self._current_file)
         
     def save_as(self, *args, **kwargs):
         file_path = asksaveasfilename(initialdir=CONFIG_DIR)
-        with open(file_path, 'w+') as f:
-            contents = text.get(1.0, END)
-            f.write(contents)
+
+        if not file_path:
+            return
+            
         self._current_file = file_path
         self.wm_title("Alarm Builder: %s" % self._current_file)
+        
+        self.save()
 
     def about(self, *args, **kwargs):
         pass
@@ -196,6 +254,9 @@ class Builder(Tk):
     def _font_change(self, *args, **kwargs):
         if not self.font_size.get().isdigit():
             self.font_size.set('12')
+        size = int(self.font_size.get())
+        if size < 8:
+            self.font_size.set('8')
         self.message_entry.config(font=(self.font_name.get(), int(self.font_size.get())))
         
     def _recenter(self, *args, **kwargs):
@@ -204,13 +265,29 @@ class Builder(Tk):
         
     def _time_change(self, *args, **kwargs):
         for var in (self.hour, self.minute):
+            # by extension checks if < 0
             if not var.get().isdigit():
                 var.set('00')
             if len(var.get()) != 2:
                 var.set(var.get()[-2:].zfill(2))
-  
- 
+        hour = int(self.hour.get())
+        minute = int(self.minute.get())
+        if hour > 23:
+            self.hour.set('23')
+        if minute > 59:
+            self.minute.set('59')
+                
+    def _warning_time_change(self, *args, **kwargs):
+        # by extension checks if < 0
+        if not self.warning_time.get().isdigit():
+            self.warning_time.set('5')
+                
+                
 if __name__ == "__main__":
+    try:
+        mkdir(GLOBALS['config_path'])
+    except:
+        pass
     root = Builder()
     root.minsize(800, 600)
     root.mainloop()
---
+++ Reminder.py
@@ -0,0 +1,271 @@
+import datetime
+import md5
+import tkFont
+import Tkinter as tk
+
+"""
+ Implement once command
+ Implement get full datetime
+"""
+
+
+# getting Tkinter fonts requires a root object but we don't want it hanging
+# around after
+def _get_font_families():
+    temp = tk.Tk()
+    fonts = sorted([x for x in tkFont.families() if not x.startswith('@')])
+    temp.destroy()
+    return fonts
+    
+DAYS = ('monday tuesday wednesday thursday friday saturday sunday '
+        'mon tue wed thu fri sat sun '
+        'mo tu we th fr sa su').split()
+MONTHS = ('january february march april may june july august september october november december '
+          'jan feb mar apr may jun jul aug sep oct nov dec').split()
+COMMANDS = 'time text font warn'.split()
+FONTS = _get_font_families()
+
+
+class ArgumentError(Exception):
+    pass
+    
+    
+class Reminder(object):
+    def __init__(self, time, days, text, warn=None, font=None):
+        for arg in 'time days text warn font'.split():
+            is_valid = getattr(self, "_is_valid_%s" % arg)
+            error_string =  is_valid(locals()[arg])
+            if error_string:
+                raise ArgumentError(error_string)
+                
+        if not warn:
+            warn = 5
+        if not font:
+            font_family = 'Arial' if 'Arial' in FONTS else FONTS[0]
+            font = (font_family, '30')
+                
+        self.__dict__.update(dict(time=time, days=days, text=text, warn=warn, font=font))
+
+    def _is_valid_time(self, time):
+        if not isinstance(time, datetime.datetime):
+            return 'time (%s) is not a datetime' % time
+    
+    def _is_valid_days(self, days):
+        if not isinstance(days, list):
+            return 'days (%s) is not a list' % days
+        if days == []:
+            return 'days cannot be empty'
+        for day in days:
+            if not isinstance(day, str):
+                return '%s in days is not a string' % day
+            if not day.lower() in DAYS:
+                return '%s in days is not one of: %s' % (day, ' '.join(DAYS))
+                
+    def _is_valid_text(self, _):
+        return
+        
+    def _is_valid_warn(self, warn):
+        if warn is None:
+            return
+        if not isinstance(warn, int):
+            return 'warn (%s) is not an integer or None' % warn
+     
+    def _is_valid_font(self, font):
+        if font is None:
+            return
+        if not isinstance(font, tuple):
+            return 'font (%s) is not a tuple or None' % font
+        if not isinstance(font[0], str):
+            return 'font family name (%s) is not a string' % font[0]
+        if not font[0] in FONTS:
+            return 'font family name (%s) is not installed on this system' % font[0]
+        if not isinstance(font[1], int):
+            return 'font size (%s) is not an integer' % font[1]
+
+
+def make_time(hours=None, minutes=None):
+    """Create a date time on Jan 1st, 2000 with variable hours and minutes
+    
+    If hours or minutes are not provided, the current time is used
+    """
+    if hours is None or minutes is None:
+        now = datetime.datetime.now()
+        hours = now.hour
+        minutes = now.minute
+    return datetime.datetime(year=2000, month=1, day=1, hour=hours, minute=minutes)
+
+def _standardize_day(day_of_week_string):
+    return DAYS[DAYS.index(day_of_week_string) % 7]
+
+def _parse_time(time_string):
+    """Parse a day and time string
+    String should be of the form
+    <day ... day> <hour>:<minute> [am/pm]
+    
+    Where:
+      day:    is the day's full name, 3 letter abbreviation, or 2 letter abbreviation
+              or one of: all, everyday, every, always
+      hour:   is 24 or 12 hour format
+              using 12 hour then requires that am/pm be given
+      minute: two digit second
+      am/pm:  is either am or pm ONLY if using 12 hour format
+      
+    Examples:
+      everyday     6:00 pm
+      Tue Wed Fri 14:00
+      Monday Fri   4:00 am
+      Fr Th Sa    23:59
+      
+    Returns:
+      datetime.datetime, ['list', 'of', 'days']          
+      The date time will always have Jan 1st, 2000 as the day
+      
+    TODO:
+      * support d/m/y with no day
+    """
+    tokens = [x.lower().strip() for x in time_string.split()]
+    
+    days = []
+    i = 0
+    # check if it repeats every day
+    if tokens[i] in ['everyday', 'every', 'all', 'always']:
+        for day in DAYS[:7]:
+            days.append(day)
+    # maybe it repeats only on certain days
+    elif tokens[i] in DAYS:
+        while tokens[i] in DAYS:
+            days.append(_standardize_day(tokens[i]))
+            i += 1
+    # maybe it is only a certain day
+    elif '/' in tokens[i]:
+        raise NotImplemented('Only certain days is not currently implemented')
+    # just assume it is every day
+    else:
+        for day in DAYS[:7]:
+            days.append(day)
+    
+    # remove the days, months, years etc so that all we have left is the time
+    tokens = tokens[i:]
+    
+    # determine if 12 hour or not
+    use_12_hour = False
+    if tokens[-1] in ['am', 'pm']:
+        use_12_hour = True
+    
+    time_split = tokens[-2 if use_12_hour else -1].split(':')
+    if len(time_split) > 2:
+        raise NotImplemented('Seconds not supported')
+    hour, minute = int(time_split[0]), int(time_split[1])
+    
+    # account for 12 hour clock
+    if use_12_hour and tokens[-1] == 'pm':
+        hour = (hour + 12) % 24
+        
+    return make_time(hour, minute), days
+    
+def _parse_font(font_string):
+    """Parse font
+    
+    string should be of the form:
+    <font family name> <font size>
+    """
+    if not font_string:
+        return None
+    return (' '.join(font_string.split()[:-1]), int(font_string.split()[-1]))
+    
+def _parse_warn(warn_string):
+    """Parse warning time line
+    
+    string should be of the form:
+    <warning minute number>
+    """
+    if not warn_string:
+        return None
+    if not warn_string.isdigit():
+        raise ArgumentError('warning minutes is not an integer')
+    return int(warn_string)
+    
+def parse_file(file_path):
+    """Parse a reminder file and return a representative object"""
+    with open(file_path) as f:
+        reminder_string = f.read()
+    return parse(reminder_string)
+    
+def parse(reminder_string):
+    """Parse a reminder string and return a representative object
+
+    File Format:
+      time: <day ... day | everyday> hour:minute [am/pm if using 12 hour format]
+      font: <font family name> <font_size>
+      warn: <minutes before warning>
+      message goes here
+      everything is taken literally
+      
+    Required File Lines:
+      # time: ...
+      Message Contents
+    
+    Returns
+      Reminder
+        time: datetime.datetime
+        days: ['list', 'of', 'days']
+        font: ("font name", int(font_size))
+        warn: <minutes before warning occurs>
+        text: "multiline message contents"
+      
+      The date time will always have Jan 1st, 2000 as the day
+    """
+    contents = reminder_string.splitlines(True)
+    
+    text = ""
+    commands = {}
+    for line in contents:
+        tokens = line.split()
+        possible_command = tokens[0].rstrip(':').lower()
+        if possible_command in COMMANDS:
+            commands[possible_command] = ' '.join(tokens[1:])
+        else:
+            text += line
+            
+    # strip the trailing newline
+    text = text[:-1]
+    
+    if not commands.get('time'):
+        raise ArgumentError('time section required in file')
+    
+    time, days = _parse_time(commands['time'])
+    font = _parse_font(commands.get('font'))
+    warn = _parse_warn(commands.get('warn'))
+
+    return Reminder(time, days, text, warn, font)
+    
+
+def serialize_file(reminder, file_path):
+    with open(file_path, 'w+') as f:
+        f.write(serialize(reminder))
+
+def serialize(reminder):
+    """Serialize a reminder object
+    
+    time will always be changed to 24 hour format
+    
+    TODO: do simplification with days
+    """
+    if not isinstance(reminder, Reminder):
+        raise ArgumentError("reminder is not a reminder object")
+        
+    contents = ""
+    
+    days_s = ' '.join(reminder.days)
+    time_s = '%s:%s' % (reminder.time.hour, reminder.time.minute)
+    contents += 'time: %s %s\n' % (days_s, time_s)
+
+    if reminder.font:
+        contents += 'font: %s %s\n' % (reminder.font[0], reminder.font[1])
+        
+    if reminder.warn:
+        contents += 'warn: %s\n' % reminder.warn
+
+    contents += reminder.text
+    
+    return contents
